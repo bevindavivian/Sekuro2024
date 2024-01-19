4d33065c07958f6a8ba5fb110db8fcf36f146909
bevindavivian <79650629+bevindavivian@users.noreply.github.com>
2024-01-20 00:15:11 +0700

    Add files via upload

---
+++ Tugas Modul 4 (ROS)/bev_workspace/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/bevinda/bev_workspace/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/bevinda/bev_workspace/devel;/opt/ros/noetic'.split(';') if '/home/bevinda/bev_workspace/devel;/opt/ros/noetic' != '' else []
