9153c179c23d4a5b3b182b5304d9f1cdeea09115
bevindavivian <79650629+bevindavivian@users.noreply.github.com>
2024-01-20 00:30:09 +0700

    Add files via upload

---
+++ Tugas Modul 4 (ROS)/bev_workspace/build/bev_package/CMakeFiles/service_server_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/bevinda/bev_workspace/devel/lib/bev_package/service_server_node"
+  "/home/bevinda/bev_workspace/devel/lib/bev_package/service_server_node.pdb"
+  "CMakeFiles/service_server_node.dir/src/service_client_node.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/service_server_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Tugas Modul 4 (ROS)/bev_workspace/build/bev_package/CMakeFiles/simple_subscriber_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/bevinda/bev_workspace/devel/lib/bev_package/simple_subscriber_node"
+  "/home/bevinda/bev_workspace/devel/lib/bev_package/simple_subscriber_node.pdb"
+  "CMakeFiles/simple_subscriber_node.dir/src/simple_subscriber_node.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simple_subscriber_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
